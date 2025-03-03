6f04e0e60519a3c58634fcc84398dfdc32ff6e07
Avinash Malik <avinash.malik@auckland.ac.nz>
2025-03-04 10:29:29 +1300

    Wrote the logical expressions and function definition and calls.
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- rules.lisp
+++ rules.lisp
@@ -53,14 +53,14 @@
 	       (cons func searched)))
       ((cons func searched)))))
 
-(defun apply-rule-set (ctx rule-set-name obj args)
+(defun apply-rule-set (ctx rule-set-name obj backend)
   (let ((func (car (progn
 		     (func-search ctx rule-set-name obj '())))))
     ;; Expected func signature is: (<nam> ctx obj &rest args)
     (assert func (rule-set-name obj)
 	          "Cannot find function to apply to ~S~% with rule: ~S"
 	          obj rule-set-name)
-    (funcall func ctx (cdr obj) args)))
+    (funcall func ctx (cdr obj) backend)))
 
 ;; --------------- Generic functions for eval------------
 (defgeneric eval-var (ctx obj backend))
@@ -75,12 +75,24 @@
 (defgeneric eval-div (ctx obj backend))
 (defgeneric eval-mod (ctx obj backend))
 (defgeneric eval-brackets (ctx obj backend))
-(defgeneric eval-ptr (ctx obj backend))
+(defgeneric eval-deref (ctx obj backend))
 (defgeneric eval-address (ctx obj backend))
 (defgeneric eval-defvar (ctx obj backend))
 (defgeneric eval-defstruct (ctx obj backend))
 (defgeneric eval-deftype (ctx obj backend))
 (defgeneric eval-block (ctx obj backend))
+(defgeneric eval-defun (ctx obj backend))
+(defgeneric eval-funcall (ctx obj backend))
+(defgeneric eval-if-else (ctx obj backend))
+
+;TODO: These still need to be implemented
+(defgeneric eval-and (ctx obj backend))
+(defgeneric eval-or (ctx obj backend))
+(defgeneric eval-lt (ctx obj backend))
+(defgeneric eval-leq (ctx obj backend))
+(defgeneric eval-gt (ctx obj backend))
+(defgeneric eval-geq (ctx obj backend))
+(defgeneric eval-leq (ctx obj backend))
 
 ;; Make a class on which the methods will be specialised
 (defclass debug-backend ()
@@ -97,14 +109,18 @@
 
 ;; -------------------------- Types --------------------
 ;; Normal types in the language -- specialised on the 3rd arg
+;; Example (type int) => int
 (defmethod eval-type (ctx obj (backend debug-backend))
   (declare (ignorable ctx))
   (format (my-stream backend) "~S " (car obj)))
 
 ;; Pointer types in the language
+;; Example: (ptrtype (structtype T)) => struct T*
 (defmethod eval-ptrtype (ctx obj (backend debug-backend))
-  (format (my-stream backend) "~S *" (car obj)))
+  (apply-rule-set ctx 'types (car obj) backend)
+  (format (my-stream backend) "*"))
 
+;; Example (structtype T) => struct T
 (defmethod eval-structype (ctx obj (backend debug-backend))
   (format (my-stream backend) "struct ~S" (car obj)))
 
@@ -163,13 +179,51 @@
   (apply-rule-set ctx 'expr (car obj) backend)
   (format (my-stream backend) ")"))
 
-(defmethod eval-ptr (ctx obj (backend debug-backend))
-  (format (my-stream backend) " * ")
+;; Example (deref x) => *x
+(defmethod eval-deref (ctx obj (backend debug-backend))
+  (format (my-stream backend) " *")
   (apply-rule-set ctx 'simple-expr (car obj) backend))
 
 (defmethod eval-address (ctx obj (backend debug-backend))
   (format (my-stream backend) "& ")
   (apply-rule-set ctx 'simple-expr (car obj) backend))
+
+;; -----------------Logical Expressions-----------
+
+(defmethod eval-and (ctx obj backend)
+  (apply-rule-set ctx 'expr (car obj) backend)
+  (format (my-stream backend) " && ")
+  (apply-rule-set ctx 'expr (cadr obj) backend))
+
+(defmethod eval-or (ctx obj backend)
+  (apply-rule-set ctx 'expr (car obj) backend)
+  (format (my-stream backend) " || ")
+  (apply-rule-set ctx 'expr (cadr obj) backend))
+
+(defmethod eval-not (ctx obj backend)
+  (format (my-stream backend) " !( ")
+  (apply-rule-set ctx 'expr (cadr obj) backend)
+  (format (my-stream backend) ")"))
+
+(defmethod eval-lt (ctx obj backend)
+  (apply-rule-set ctx 'expr (car obj) backend)
+  (format (my-stream backend) " < ")
+  (apply-rule-set ctx 'expr (cadr obj) backend))
+
+(defmethod eval-leq (ctx obj backend)
+  (apply-rule-set ctx 'expr (car obj) backend)
+  (format (my-stream backend) " <= ")
+  (apply-rule-set ctx 'expr (cadr obj) backend))
+
+(defmethod eval-gt (ctx obj backend)
+  (apply-rule-set ctx 'expr (car obj) backend)
+  (format (my-stream backend) " > ")
+  (apply-rule-set ctx 'expr (cadr obj) backend))
+
+(defmethod eval-geq (ctx obj backend)
+  (apply-rule-set ctx 'expr (car obj) backend)
+  (format (my-stream backend) " >= ")
+  (apply-rule-set ctx 'expr (cadr obj) backend))
 
 ;;------------------ Statements -----------------
 (defmethod eval-assign (ctx obj (backend debug-backend))
@@ -180,7 +234,6 @@
 
 (defmethod eval-seq (ctx obj (backend debug-backend))
   (apply-rule-set ctx 'stmt (nth 0 obj) backend)
-  ;; (format (my-stream backend) ";~%")
   (apply-rule-set ctx 'stmt (nth 1 obj) backend))
 
 (defmethod eval-defvar (ctx obj (backend debug-backend))
@@ -195,6 +248,10 @@
   (apply-rule-set ctx 'stmt (cadr obj) backend)
   (format (my-stream backend) ";~%"))
 
+;; Example (deftype (structype T) (block (defvar (type int) (var y))))
+;; => struct T {int y;};
+;; Example 2 (deftype (structtype T1) (block (seq (defvar (int (var y))) (defvar (ptrtype (type float)) (var m)))))
+;; => struct T1 {int y; float* m;};
 (defmethod eval-deftype (ctx obj (backend debug-backend))
   (apply-rule-set ctx 'struct-type (car obj) backend)
   (format (my-stream backend) " ~S " (cadr obj))
@@ -203,8 +260,51 @@
 (defmethod eval-block (ctx obj (backend debug-backend))
   (format (my-stream backend) "{~%")
   (apply-rule-set ctx 'stmt (car obj) backend)
-  (format (my-stream backend) "}~%")
-  (format (my-stream backend) ";~%"))
+  (format (my-stream backend) "}~%"))
+
+;; Example (defun F (((type int) (var x)) ((ptrtype (type float)) (var m))) (type int) (block (seq (assign (var x) (plus (var x) (val 10))) (assign (ptr (var m)) (val 10)))))
+;; => F(int x, float* m){x = x + 1; *m = 10;}
+(defmethod eval-defun (ctx obj (backend debug-backend))
+  ;; Define the output type
+  (apply-rule-set ctx 'types (third obj) backend)
+  (format (my-stream backend) (first obj))
+  ;; Get all the parameters in a string
+  (format (my-stream backend) "(")
+  (loop for i in (second obj) do
+    (progn
+      (apply-rule-set ctx 'types (car i) backend)
+      (apply-rule-set ctx 'expr (cadr i) backend)
+      (when (not (eq i (last (second obj))))
+	(format (my-stream backend) ", "))))
+  (format (my-stream backend) ")")
+  ;; define the body of the function
+  (apply-rule-set ctx 'stmt (last obj) backend))
+
+;; This should be an expression
+;; Example (funcall f ((var x) (var y)) (block (...)))
+(defmethod eval-funcall (ctx obj (backend debug-backend))
+  (format (my-stream backend) "~S" (car obj))
+  ;; loop through the function call arguments
+  (format (my-stream backend) "(")
+  (loop for i in (second obj)
+	for count from 0
+	if (< count (1- (length (second obj)))) do
+	  (apply-rule-set ctx 'expr i backend)
+	  (format (my-stream backend) ", ")
+	else do
+	  (apply-rule-set ctx 'expr i backend))
+  (format (my-stream backend) ")")
+  (apply-rule-set ctx 'stmt (last obj) backend))
+
+;; Example (if-else (logical-expr x) (block ) (block))
+;FIXME: Still need to add the logical-expr
+(defmethod eval-if-else (ctx obj (backend debug-backend))
+  (format (my-stream backend) "if (")
+  (apply-rule-set ctx 'logical-expr (car obj) backend)
+  (format (my-stream backend) ")")
+  (apply-rule-set ctx 'stmt (second obj) backend)
+  (format (my-stream backend) " else ")
+  (apply-rule-set ctx 'stmt (third obj) backend))
 
 ;; We first instantite the context
 (defvar ctx)
